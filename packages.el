81f483c403852968e904e6e6e8338fc09da3b158
Samuel Nilsson <samuel@samuelnilsson.net>
2025-03-06 21:23:31 +0100

    enable mermaid mode.

--- packages.el
+++ packages.el
@@ -2,5 +2,6 @@
 (package! copilot
   :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
 (package! guess-language)
+(package! mermaid-mode)
 (package! mermaid-ts-mode)
 (package! ob-mermaid)
