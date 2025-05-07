8d838732825d8ab1b5c5abcc6fb53a3f789b5c59
Samuel Nilsson <samuel@samuelnilsson.net>
2025-05-07 19:36:50 +0200

    add present flag to org module.

--- init.el
+++ init.el
@@ -152,7 +152,7 @@
        ;;nim               ; python + lisp at the speed of c
        (nix +tree-sitter +lsp)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2 +pretty)               ; organize your plain life in plain text
+       (org +roam2 +pretty +present)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
