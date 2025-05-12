6312ca1f538665bdf9cf5fa7e36e588f9451c838
Samuel Nilsson <samuel@samuelnilsson.net>
2025-05-12 07:44:48 +0200

    structure todo list using org-super-agenda.

--- packages.el
+++ packages.el
@@ -5,6 +5,7 @@
 (package! mermaid-mode)
 (package! mermaid-ts-mode)
 (package! ob-mermaid)
+(package! org-super-agenda)
 (unless (string= "" (getenv "DOOM_CALDAV_SERVER"))
   (package! org-caldav))
 (unless (string= "" (getenv "DOOM_JIRA_URL"))
